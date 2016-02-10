3f94a43ad935f82ec5a7c17123265cd742f675e1
ribrciti <rbciti@msn.com>
2016-02-10 11:15:02 -0800

    Added drop down selection box for invoices status_type with migration for status type

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160209172654) do
+ActiveRecord::Schema.define(version: 20160210165655) do
 
   create_table "companies", force: :cascade do |t|
     t.string   "name",       limit: 255
@@ -29,6 +29,7 @@ ActiveRecord::Schema.define(version: 20160209172654) do
     t.string   "salesperson", limit: 255
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
+    t.string   "status_type", limit: 255
   end
 
   create_table "users", force: :cascade do |t|
